42f6bc24cf9c808f64ddc7a392d9197babbfd492
Antonin Sarrazin <107463703+a-sarrazin@users.noreply.github.com>
2022-08-12 15:07:51 +0200

    Add examples folder and ROADMAP.md

---
+++ examples/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    hidora = {
+        source = "HidoraSwiss/hidora"
+    }
+  }
+}
+
+provider "hidora" {
+    host = "app.hidora.com"
+    access_token = "1a7904b00e4c4b719d8f2956e1287dda075503cf"
+}
+
+data "hidora_create_env" "test-data" {
+    id = "env-6378398"
+}
+
+resource "hidora_create_env" "test-res" {
+    envgroups = "${data.hidora_create_env.test-data.envgroups}" // Can be update
+    environment {
+        ishaenabled = true
+        region = "new" // Can be update
+        shortdomain = "env-made-by-terraform" // Force new instance and destroy previous instance !
+        sslstate = true
+    }
+    nodes {
+        count = 2
+        disklimit = 50
+        nodegroup = "cp"
+        nodetype = "apache-python"
+        env = {
+            TEST = "test"
+        }
+        extip = false
+        fixedcloudlets = 4
+        flexiblecloudlets = 8
+        image = "python"
+        mission = "cp"
+        tag = "latest"
+    }
+}
